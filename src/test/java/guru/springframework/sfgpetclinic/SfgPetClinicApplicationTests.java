b8d7c7c83415ccc7c25b46ac9dbcd8bc25cb417c
Jorge B. Dutton <jorge.dutton@gmail.com>
2020-04-13 09:17:01 +0200

    Renamed Application and ApplicationTest

--- src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,10 +1,13 @@
 package guru.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class DemoApplicationTests {
+class SfgPetClinicApplicationTests {
 
 	@Test
 	void contextLoads() {
