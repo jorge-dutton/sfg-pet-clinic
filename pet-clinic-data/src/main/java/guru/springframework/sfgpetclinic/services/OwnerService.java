64dd6ca0a4bf46fdccd51fe597401b7225f31314
Jorge Dutton <jorge.dutton@gmail.com>
2020-05-20 08:52:27 +0200

    Closes SFG-010

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -1,11 +1,8 @@
 package guru.springframework.sfgpetclinic.services;
 
-import java.util.Set;
 import guru.springframework.sfgpetclinic.model.Owner;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 	Owner findByLastName(String lastName);
-	Owner findById(Long id);
-	Owner save(Owner owner);
-	Set<Owner> findAll();
+
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,11 +1,8 @@
 package guru.springframework.sfgpetclinic.services;
 
-import java.util.Set;
 import guru.springframework.sfgpetclinic.model.Vet;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
 	Vet findByLastName(String lastName);
-	Vet findById(Long id);
-	Vet save(Vet vet);
-	Set<Vet> findAll();
+
 }
