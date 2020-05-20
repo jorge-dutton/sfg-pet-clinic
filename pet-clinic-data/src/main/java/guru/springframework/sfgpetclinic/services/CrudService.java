e37b62a9881e87157ceae7ac79ffbb20c62f1182
Jorge Dutton <jorge.dutton@gmail.com>
2020-05-20 09:55:39 +0200

    Closes #11

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -2,6 +2,14 @@ package guru.springframework.sfgpetclinic.services;
 
 import java.util.Set;
 
+/**
+ * Common interface similar to CrudRepository
+ * 
+ * @author jorge.dutton
+ *
+ * @param <T>
+ * @param <ID>
+ */
 public interface CrudService<T, ID> {
 	Set<T> findAll();
 	T findById(ID id);
