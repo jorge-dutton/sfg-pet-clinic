904117a8daf5429dadefd457b9d5e320ea7a5725
Jorge Dutton <jorge.dutton@gmail.com>
2020-05-20 12:30:59 +0200

    closes #6

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,38 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long>
+		implements
+			CrudService<Owner, Long> {
+
+	@Override
+	public Set<Owner> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public void delete(Owner object) {
+		super.deleteByObject(object);
+
+	}
+
+}
